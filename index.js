834ea6cc93e1f5911fb2880975eae70eb9043f03
PanaddaManarata <manarata.panadda@gmail.com>
2019-08-01 17:01:52 +0200

    last time

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 window.onload = function(e){ 
    console.log('hi');
 
-
-
 let btnLeft = document.getElementById('buttonleft'); 
-console.log(btnLeft);
 let btnRight = document.getElementById('buttonright'); 
 let Carrousel = []; 
-
-Carrousel = ["cocktails.jpg", "ginto.jpg", "coctail_material.jpg", "pinkcocktail.jpg"]; 
+var clickCounter = 0; 
+Carrousel = ["img/cocktails.jpg", "img/ginto.jpg", "img/coctail_material.jpg", "img/pinkcocktail.jpg"]; 
     
     
 
@@ -18,27 +15,73 @@ Carrousel = ["cocktails.jpg", "ginto.jpg", "coctail_material.jpg", "pinkcocktail
     let contactBtn = document.getElementById('contacttab'); 
     var summaryTab = document.getElementById('summary'); 
     var infoTab = document.getElementById('information');
-    var contactTab = document.getElementById('') 
+    var contactTab = document.getElementById('contact') 
     
 
     summaryBtn.addEventListener('click', summaryClick)
-    
-    function summaryClick(){
-        console.log("hi sum");
-
-    }
-
     informationBtn.addEventListener('click', infoClick)
+    contactBtn.addEventListener('click', contactClick)
+
+    
     
     function infoClick(){
         console.log("hi info");
+        summaryTab.style.display = "none"; 
+        infoTab.style.display = "block"; 
+        contactTab.style.display = "none"; 
+
     }
 
-    contactBtn.addEventListener('click', contactClick)
+   
     
     function contactClick(){
         console.log("hi contact");
+        summaryTab.style.display = "none"; 
+        infoTab.style.display = "none"; 
+        contactTab.style.display = "block"; 
     }
+
+    function summaryClick(){
+        console.log("hi sum");
+        summaryTab.style.display = "block"; 
+        infoTab.style.display = "none"; 
+        contactTab.style.display = "none"; 
+
+    }
+
+function timeToParty(){
+    var countDownDate = new Date("Aug 5, 2019 15:37:25").getTime();
+    console.log(countDownDate)
+    var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+      
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+      
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+        // Display the result in the element with id="demo"
+        document.getElementById("timetoparty").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+      
+        // If the count down is finished, write some text 
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("timetoparty").innerHTML = "EXPIRED";
+        }
+      }, 1000);
+}
+
+timeToParty(); 
+
+
+
 
 
 
